1abfe2548c3a244e9d7a63cbbadc3210d5ca9707
Tara Evans <tarajevans@hotmail.com>
2022-10-15 20:07:54 -0400

    testing

---
+++ src/components/about/__tests__/index.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import About from '..';
+
+const { asFragment } = render(<About />);
+
+
+
+afterEach(cleanup);
+
+describe('About component', () => {
+    // First Test
+    it('renders', () => {
+      render(<About />);
+    });
+  
+    // Second Test
+    it('matches snapshot DOM node structure', () => {
+        // render About
+        expect(asFragment()).toMatchSnapshot();
+      });
+  })
