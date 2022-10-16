88cf9843f067a22336dd5eb4cfe38223b322770d
Tara Evans <tarajevans@hotmail.com>
2022-10-16 15:33:11 -0400

    contact and navactive

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -7,6 +7,8 @@ function Nav(props) {
     categories = [],
     setCurrentCategory,
     currentCategory,
+    contactSelected,
+    setContactSelected
   } = props;
 
   useEffect(() => {
@@ -22,26 +24,26 @@ function Nav(props) {
       </h2>
       <nav>
         <ul className="flex-row">
-          <li className="mx-2">
-            <a data-testid="about" href="#about">
+        <li className={`mx-2 ${contactSelected && 'navActive'}`}>
+          <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>
               About me
             </a>
           </li>
           <li className="mx-2">
-            <span>Contact</span>
+          <span onClick={() => setContactSelected(true)}>Contact</span>
           </li>
           {categories.map((category) => (
             <li
-              className={`mx-1 ${
-                currentCategory.name === category.name && 'navActive'
-                }`}
-              key={category.name}
+            className={`mx-1 ${
+              currentCategory.name === category.name && !contactSelected && `navActive`
+              }`}
+            key={category.name}
+          >
+              <span onClick={() => {
+                setCurrentCategory(category);
+                setContactSelected(false);
+              }}
             >
-              <span
-                onClick={() => {
-                  setCurrentCategory(category)
-                }}
-              >
                 {capitalizeFirstLetter(category.name)}
               </span>
             </li>
